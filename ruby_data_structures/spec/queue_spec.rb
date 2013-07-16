b6f12d5f692ea3f5f278559d0e7d275f1b8f84f9
Hack Reactor Students <github@hackreactor.com>
2013-07-16 10:49:43 -0700

    Added initialization for queue, queue spec for this passes.

--- ruby_data_structures/spec/queue_spec.rb
+++ ruby_data_structures/spec/queue_spec.rb
@@ -1,5 +1,11 @@
 require './src/queue.rb'
 
 describe Queue do
-	
+  before(:each) do
+    @queue = Queue.new
+  end
+
+  it "initializes empty" do
+    @queue.length.should == 0
+  end
 end
--- ruby_data_structures/src/queue.rb
+++ ruby_data_structures/src/queue.rb
@@ -1,4 +1,10 @@
 class Queue
   def initialize
+    @length = 0
+    @storage = []
+  end
+
+  def length
+    @length
   end
 end
